4ca4970545c6d0bdf3dbbbbaea8f0a633b0573b8
Griffin <griffinquarles11@gmail.com>
2023-02-01 12:40:30 -0600

    final commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getLocation} from "./modules/data";
+import { getLocationFarenheit, getLocationCelsius} from "./modules/data";
 const searchBar = document.getElementById('search');
 const form = document.querySelector('form');
 const location = document.querySelector('.location');
@@ -10,34 +10,60 @@ const humidity = document.querySelector('.humidity');
 const wind = document.querySelector('.wind');
 const description = document.querySelector('.description');
 const weatherPic = document.querySelector('.weatherPic');
-
+const toggleButton = document.querySelector('.toggleButton');
 
 
 async function displayData(city){
     try{
-        const response = await getLocation(city);
-        location.innerText = response.name;
-        temperature.innerText = Math.round(response.temp) + "\u00B0F";
-        low.innerText = 'Low: ' + Math.round(response.low) + "\u00B0F";
-        high.innerText = 'High: ' + Math.round(response.high) + "\u00B0F";
-        feelLike.innerText = 'Feels like: ' + Math.round(response.feelsLike) + "\u00B0F";
-        humidity.innerText = 'Humidity: ' + Math.round(response.humidity)+ '%';
-        wind.innerText = 'Wind Speed: ' + response.wind.toFixed(1) +'mph';
-        description.innerText = response.weather;
-        if(response.weather === 'Clouds'){
-            weatherPic.src = '../dist/imgs/cloudyDay.png';
-        }else if (response.weather === 'Clear'){
-            weatherPic.src = '../dist/imgs/sunWeather.png';
-        }else if(response.weather === 'Mist'){
-            weatherPic.src = '../dist/imgs/mistPic.png';
-        }else if (response.weather === 'Rain'){
-            weatherPic.src = '../dist/imgs/rainyDay.png';
-        }else if (response.weather === 'Snow'){
-            weatherPic.src = '../dist/imgs/snowyDay.png';
-        }else{
-            weatherPic.src = '';
+        if(toggleButton.innerHTML === 'F'){
+            const response = await getLocationFarenheit(city);
+            location.innerText = response.name;
+            temperature.innerText = Math.round(response.temp) + "\u00B0F";
+            low.innerText = 'Low: ' + Math.round(response.low) + "\u00B0F";
+            high.innerText = 'High: ' + Math.round(response.high) + "\u00B0F";
+            feelLike.innerText = 'Feels like: ' + Math.round(response.feelsLike) + "\u00B0F";
+            humidity.innerText = 'Humidity: ' + Math.round(response.humidity)+ '%';
+            wind.innerText = 'Wind Speed: ' + response.wind.toFixed(1) +'mph';
+            description.innerText = response.weather;
+            if(response.weather === 'Clouds'){
+                weatherPic.src = '../dist/imgs/cloudyDay.png';
+            }else if (response.weather === 'Clear'){
+                weatherPic.src = '../dist/imgs/sunWeather.png';
+            }else if(response.weather === 'Mist'){
+                weatherPic.src = '../dist/imgs/mistPic.png';
+            }else if (response.weather === 'Rain'){
+                weatherPic.src = '../dist/imgs/rainyDay.png';
+            }else if (response.weather === 'Snow'){
+                weatherPic.src = '../dist/imgs/snowyDay.png';
+            }else{
+                weatherPic.src = '';
         }
-    }catch(err){
+        }else{
+            const response = await getLocationCelsius(city);
+            location.innerText = response.name;
+            temperature.innerText = Math.round(response.temp) + "\u00B0C";
+            low.innerText = 'Low: ' + Math.round(response.low) + "\u00B0C";
+            high.innerText = 'High: ' + Math.round(response.high) + "\u00B0C";
+            feelLike.innerText = 'Feels like: ' + Math.round(response.feelsLike) + "\u00B0C";
+            humidity.innerText = 'Humidity: ' + Math.round(response.humidity)+ '%';
+            wind.innerText = 'Wind Speed: ' + response.wind.toFixed(1) +'mph';
+            description.innerText = response.weather;
+            if(response.weather === 'Clouds'){
+                weatherPic.src = '../dist/imgs/cloudyDay.png';
+            }else if (response.weather === 'Clear'){
+                weatherPic.src = '../dist/imgs/sunWeather.png';
+            }else if(response.weather === 'Mist'){
+                weatherPic.src = '../dist/imgs/mistPic.png';
+            }else if (response.weather === 'Rain'){
+                weatherPic.src = '../dist/imgs/rainyDay.png';
+            }else if (response.weather === 'Snow'){
+                weatherPic.src = '../dist/imgs/snowyDay.png';
+            }else{
+                weatherPic.src = '';
+            }
+            }
+        }
+catch(err){
 
     }
     
@@ -48,4 +74,17 @@ form.addEventListener('submit',(event)=> {
     const info = searchBar.value;
     displayData(info);
     searchBar.value = '';
+})
+
+toggleButton.addEventListener('click', ()=>{
+    if(toggleButton.innerHTML === 'F'){
+        toggleButton.innerHTML = 'C';
+        const city = location.innerHTML;
+        displayData(city);
+        
+    }else{
+        toggleButton.innerHTML = 'F';
+        const city = location.innerHTML;
+        displayData(city);
+    }
 })
--- src/modules/data.js
+++ src/modules/data.js
@@ -1,6 +1,27 @@
-export const getLocation = async function (location){
+export const getLocationFarenheit = async function (location){
     try{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f824c9a258641470aebe27d4e0e686d3&units=imperial`
+    , {mode:"cors"});
+    const json = await response.json();
+    const weatherData = {
+        name: json.name,
+        temp: json.main.temp,
+        feelsLike: json.main.feels_like,
+        humidity: json.main.humidity,
+        low: json.main.temp_min,
+        high: json.main.temp_max,
+        weather: json.weather[0].main,
+        wind: json.wind.speed
+    };
+    return weatherData;
+    }catch(err){
+        alert('City not found Try again');
+    }
+    
+}
+export const getLocationCelsius = async function (location){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f824c9a258641470aebe27d4e0e686d3&units=metric`
     , {mode:"cors"});
     const json = await response.json();
     const weatherData = {
